13c8b3c21a34fe97b013d917d420f661e9c2bcdc
Danny Lu <dannylu@dannys-mbp.attlocal.net>
2018-03-27 15:51:44 -0700

    Pseudo implementation of botnets

---
+++ flask_server.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send, emit
+
+app = Flask(__name__)
+app.debug = True
+app.config['Secret Key'] = 'dannysecret'
+socketio = SocketIO(app)
+
+users = {}
+num_of_users = 0
+
+base_string = str(input("Enter base string: "))
+work_range = int(input("Enter work range: "))
+target = str(input("Enter the target number of leading zeros in the hash"))
+current_count = -1
+
+@app.route('/')
+def index():
+    return render_template("front_end.html")
+
+@socketio.on("connection")
+def handle_connection(conn):
+    #message = str(current_count + 1) + '-' + str(current_count + work_range) + '-' + target + '-' + base_string
+    print("Received: " + conn);
+
+@socketio.on("payload")
+def handle_payload(pl):
+    print("Payload in: " + pl)
+    emit("payload_in", pl, broadcast = True)
+
+if __name__ == "__main__":
+    socketio.run(app)
